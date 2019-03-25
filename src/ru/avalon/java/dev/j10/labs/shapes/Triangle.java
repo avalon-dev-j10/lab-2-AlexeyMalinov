5ed1ebe9eaf1dbd24d03cbb83ba2b175a306eb6f
AlexeyMalinov <alexeymalinov@yandex.ru>
2019-03-25 14:48:17 +0300

    Выполнены все задачи связанные с созданием класса Triangle

--- src/ru/avalon/java/dev/j10/labs/shapes/Triangle.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Triangle.java
@@ -14,11 +14,57 @@ package ru.avalon.java.dev.j10.labs.shapes;
  *
  * @see <a href="https://ru.wikipedia.org/wiki/%D0%A2%D1%80%D0%B5%D1%83%D0%B3%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA">Треугольник</a>
  */
-public class Triangle {
+public class Triangle implements Polygon {
 
-    /*
-     * TODO: Реализовать класс 'Triangle'
-     * 1. Используйте наследование.
-     * 2. Реализуйте все абстрактные методы.
+    private final Point a;
+    private final Point b;
+    private final Point c;
+    private final int rotation;
+
+    public Triangle(Point a, Point b, Point c, int rotation) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.rotation = rotation;
+    }
+
+    public Triangle(Point a, Point b, Point c) {
+        this(a, b, c, 0);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return
      */
+    @Override
+    public float getPerimeter() {
+        return a.distanceTo(b) + b.distanceTo(c) + c.distanceTo(a);
+    }
+
+    /**
+     * {@inheritDoc}
+     * Площадь вычисляется по формуле Герона
+     *
+     * @return
+     */
+    @Override
+    public float getArea() {
+        float halfPerimeter = getPerimeter() / 2;
+        return (float) Math.sqrt(halfPerimeter *
+                (halfPerimeter - a.distanceTo(b)) *
+                (halfPerimeter - b.distanceTo(c)) *
+                (halfPerimeter - c.distanceTo(a)));
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return
+     */
+    @Override
+    public int getRotation() {
+        return rotation;
+    }
+
 }
